ed84af94645c7ea7cb85ef199597db8361b6660c
Czar <graficx@gmail.com>
2013-04-20 15:24:02 -0500

    Backup joomla
    Primer backup

---
+++ logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2013-04-16 06:48:43 UTC
+#Software: Joomla Platform 12.2.0 Stable [ Neil Armstrong ] 21-September-2012 00:00 GMT
+
+#Fields: datetime	priority	category	message
+2013-04-16T06:48:43+00:00	INFO	joomlafailure	Username and password do not match or you do not have an account yet.
